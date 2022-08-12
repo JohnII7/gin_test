71bbcd68fea7b739ee8dabdee6c67730179500fc
JohnII7 <157798378@qq.com>
2022-08-12 12:54:36 +0800

    gin之获取参数绑定

--- main.go
+++ main.go
@@ -6,25 +6,50 @@ import (
 	"net/http"
 )
 
+type UserInfo struct {
+	Username string `form:"username"`
+	Password string `form:"password"`
+}
+
 func main() {
 	r := gin.Default()
-	r.GET("/:name/:age", func(c *gin.Context) {
-		name := c.Param("name")
-		age := c.Param("age")
-		c.JSON(http.StatusOK, gin.H{
-			"name": name,
-			"age":  age,
-		})
-	})
-	r.GET("/blog/:year/:month", func(c *gin.Context) {
-		year := c.Param("year")
-		month := c.Param("month")
-		c.JSON(http.StatusOK, gin.H{
-			"year":  year,
-			"month": month,
-		})
+	r.GET("/user", func(c *gin.Context) {
+		//username := c.Query("username")
+		//password := c.Query("password")
+		//u := UserInfo{
+		//	username: username,
+		//	password: password,
+		//}
+		var u UserInfo
+		err := c.ShouldBind(&u)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+		} else {
+			// %#v先打印结构体名字，再打印结构体
+			fmt.Printf("%#v\n", u)
+			c.JSON(http.StatusOK, gin.H{
+				"status": "ok",
+			})
+		}
 	})
 
+	r.POST("/form", func(c *gin.Context) {
+		var u UserInfo
+		err := c.ShouldBind(&u)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+		} else {
+			// %#v先打印结构体名字，再打印结构体
+			fmt.Printf("%#v\n", u)
+			c.JSON(http.StatusOK, gin.H{
+				"status": "ok",
+			})
+		}
+	})
 	err := r.Run(":9090")
 	if err != nil {
 		fmt.Println("err:", err)
